4e1bd9e6aaed3bdad922666365c6486a7096685d
vikpa <panasvik@cvut.cz>
2026-08-25 22:22:50 +0200

    fileRWM: AddReader removed bug fixes

--- internal/caching/fileRWM.go
+++ internal/caching/fileRWM.go
@@ -2,11 +2,16 @@ package caching
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"sync"
 )
 
-type FileState struct {
+var (
+	ErrImgNotFound = errors.New("img not found")
+)
+
+type fileState struct {
 	readers  int  // количество текущих горутин-читателей
 	toDelete bool // флаг, что файл заказан на удаление
 }
@@ -14,95 +19,114 @@ type FileState struct {
 // CacheTable TODO: implement states into writeBehind, cacheCleaner, workers DONE
 type CacheTable struct {
 	mu            sync.Mutex
-	items         map[string]FileState
+	items         map[string]fileState
 	diskSemaphore chan struct{}
 }
 
 func NewTable() *CacheTable {
 	return &CacheTable{
-		items:         make(map[string]FileState),
+		items:         make(map[string]fileState),
 		diskSemaphore: make(chan struct{}, 10),
 	}
 }
 
-func (c *CacheTable) AddReader(path string) {
+func (c *CacheTable) AddFileState(path string, numReaders int) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	state, exists := c.items[path]
-	if !exists {
-		state = c.AddFileState(path, 0)
-	}
-	state.readers++
-	c.items[path] = state
-}
-
-func (c *CacheTable) AddFileState(path string, numReaders int) FileState {
-	fs := FileState{numReaders, false}
+	fs := fileState{numReaders, false}
 	c.items[path] = fs
-	return fs
 }
 
 func (c *CacheTable) CacheLockFile(path string) error {
 	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	state, exists := c.items[path]
 
 	if !exists || state.toDelete {
-		c.mu.Unlock()
 		return errors.New("file not found or being deleted")
 	}
 	c.diskSemaphore <- struct{}{}
 	state.readers++
 	c.items[path] = state
-	c.mu.Unlock()
 	return nil
 }
 
 func (c *CacheTable) CacheUnlockFile(path string) {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	st := c.items[path]
 	st.readers--
 	c.items[path] = st
 
 	needDelete := st.toDelete && st.readers == 0
-	if needDelete {
-		delete(c.items, path)
-	}
-	c.mu.Unlock()
 
-	if needDelete {
-		os.Remove(path)
-	}
-	<-c.diskSemaphore
-
+	defer func() {
+		if needDelete {
+			os.Remove(path)
+		}
+	}()
+	defer func() { <-c.diskSemaphore }()
 }
 
 func (c *CacheTable) CleanUpFile(path string) error {
 	c.diskSemaphore <- struct{}{}
 	defer func() { <-c.diskSemaphore }()
-
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	state, exists := c.items[path]
 
 	if !exists || state.toDelete {
-		c.mu.Unlock()
 		return nil
 	}
 
 	if state.readers == 0 {
 		delete(c.items, path)
-		c.mu.Unlock()
 		return os.Remove(path)
 	}
 
 	state.toDelete = true
 	c.items[path] = state
-	c.mu.Unlock()
 
 	return nil
 }
 
+func (c *CacheTable) ReadCacheFile(path string) ([]byte, error) {
+	state, exists := c.items[path]
+	if !exists {
+		return nil, fmt.Errorf("cache table: %v", ErrImgNotFound)
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	state.readers++
+	defer func() { c.items[path] = state }()
+	defer func() { state.readers-- }()
+	c.diskSemaphore <- struct{}{}
+	defer func() { <-c.diskSemaphore }()
+	return os.ReadFile(path)
+}
+
+// ReadFile for NOT cache files, for cache files use ReadCacheFile
 func (c *CacheTable) ReadFile(path string) ([]byte, error) {
 	c.diskSemaphore <- struct{}{}
 	defer func() { <-c.diskSemaphore }()
 	return os.ReadFile(path)
+}
+
+func (c *CacheTable) WriteFile(path string, data []byte) error {
+	c.diskSemaphore <- struct{}{}
+	defer func() { <-c.diskSemaphore }()
+	return os.WriteFile(path, data, 0644)
+}
+
+func (c *CacheTable) Remove(path string) error {
+	c.diskSemaphore <- struct{}{}
+	defer func() { <-c.diskSemaphore }()
+	return os.Remove(path)
+}
+
+func (c *CacheTable) Exists(path string) bool {
+	_, exists := c.items[path]
+	return exists
 }
