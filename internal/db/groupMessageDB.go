a402a5da814f56c933b94edaae1b39e1e5b68ec6
vikpa <panasvik@cvut.cz>
2026-09-18 20:37:10 +0200

    added ability to invite user by the username, minor fixes

--- internal/db/groupMessageDB.go
+++ internal/db/groupMessageDB.go
@@ -102,7 +102,7 @@ func (db *MessageDB) ProcessEvent(e brocker.Event) util.Issue {
 			data.MessageID = msgID
 			e.Forward(data, brocker.SendMessage)
 			if data.MsgType == util.Consent {
-				body := util.UserGroup{data.RecipientID, data.GroupID, "user"}
+				body := util.UserGroup{data.SenderID, data.GroupID, "user"}
 				e.Forward(body, brocker.AddUserToGroup)
 			}
 		}
